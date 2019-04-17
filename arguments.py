e3efbe5fcd774ffaaa5eb761966990a11a225ae8
Jahlia Finney <finneyj@allegheny.edu>
2019-04-17 18:44:11 -0400

    started arguments

--- arguments.py
+++ arguments.py
@@ -6,4 +6,7 @@ def parse(args):
     irb_parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    
+
+    irbparser.add_argument(
+        "--checklists", help="prints checklist items", action="store_true"
+    )
