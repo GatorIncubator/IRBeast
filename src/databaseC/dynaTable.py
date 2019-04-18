207637e704bc98e58390fc33c41c55b86ad3fcca
Jahlia Finney <finneyj@allegheny.edu>
2019-04-18 14:19:57 -0400

    added skeleton

--- src/databaseC/dynaTable.py
+++ src/databaseC/dynaTable.py
@@ -1,4 +1,11 @@
 from __future__ import print_function
 import boto3
 
-dynamodb = boto3.resource('dynamodb',)
+dynamodb = boto3.resource('dynamodb', region_name = "us-west-2", endpoint_url= "http://localhost:8000")
+
+table = dynamob .create_table(
+    TableName = "Irb",
+    Keyschema = [
+
+    ]
+)
