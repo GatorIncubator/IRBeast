5f2e02cf466121801bed75a9f604bf1c37516777
Jahlia Finney <finneyj@allegheny.edu>
2019-04-23 18:08:56 -0400

    made some corrections

--- src/databaseC/dynaTable.py
+++ src/databaseC/dynaTable.py
@@ -1,36 +1,23 @@
 from __future__ import print_function
 import boto3
 
-dynamodb = boto3.resource('dynamodb', region_name = "us-west-2", endpoint_url= "http://localhost:8000")
-
-table = dynamob .create_table(
-    TableName = "Irb",
-    Keyschema = [
-    {
-        'AttributeName': 'studentIDNumber',
-        'KeyType': 'HASH'
-    }
-    {
-        'AttributeName: 'name',
-        'KeyType': 'sort'
-    }
-    {
-        'AttributeName: 'major'
-    }
-],
-    AttributeDefinitions= [
-    {
-        'AttributeName': 'year',
-        'AttributeType': 'N'
-    }
-    {
-        'AttributeName': 'title',
-        'AttributeType: 'Y'
-    },
-     ProvisionedThroughput={
-        'ReadCapacityUnits': 10,
-        'WriteCapacityUnits': 10
-    }
-    ]
-
+dynamodb = boto3.resource(
+    "dynamodb", region_name="us-west-2", endpoint_url="http://localhost:8000"
 )
+
+table = dynamodb.create_table(
+    TableName="Irb",
+    KeySchema=[
+        {"AttributeName": "studentIDNumber", "KeyType": "HASH"},
+        {"AttributeName": "major", "KeyType": "RANGE"},
+    ],
+    AttributeDefinitions=[
+        {"AttributeName": "year", "AttributeType": "N"},
+        {"AttributeName": "title", "AttributeType": "Y"},
+    ],
+    ProvisionedThroughput={
+    "ReadCapacityUnits": 10,
+    "WriteCapacityUnits": 10},
+)
+
+print("Table status:", table.table_status)
