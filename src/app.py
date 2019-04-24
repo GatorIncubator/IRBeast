eec0a880c4fec7c5e69349f695cf90ac1dec8eb4
Jahlia Finney <finneyj@allegheny.edu>
2019-04-24 15:51:49 -0400

    updated the app.py file

---
+++ src/app.py
@@ -0,0 +1,53 @@
+import os
+
+import boto3
+from chalice import Chalice
+from chalicelib import db
+from chalicelib import rekognition
+
+app = Chalice(app_name='IRBeast')
+
+_MEDIA_DB = None
+_REKOGNITION_CLIENT = None
+
+
+def get_media_db():
+    global _MEDIA_DB
+    if _MEDIA_DB is None:
+        _MEDIA_DB = db.DynamoMediaDB(
+            boto3.resource('dynamodb').Table(
+                os.environ['Irb']))
+    return _MEDIA_DB
+
+@app.lambda_function()
+def detect_labels_on_image(event, context):
+    bucket = event['Bucket']
+    key = event['Key']
+    labels = get_rekognition_client().get_image_labels(bucket=bucket, key=key)
+    get_media_db().add_media_file(key, media_type=db.IMAGE_TYPE, labels=labels)
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
