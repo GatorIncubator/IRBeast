b56137814ad5f9b29c6ec19d32a4310e4005223d
Zac Shaffer <shafferz@allegheny.edu>
2019-05-01 18:41:33 -0400

    Fixed pylint errors for app module.

--- src/app.py
+++ src/app.py
@@ -1,3 +1,7 @@
+"""
+The APP module handles functionality for connecting to the
+AWS Chalice database.
+"""
 import os
 
 import boto3
@@ -5,23 +9,27 @@ from chalice import Chalice
 from chalicelib import db
 from chalicelib import rekognition
 
-app = Chalice(app_name="IRBeast")
+APP = Chalice(app_name="IRBeast")
 
 _MEDIA_DB = None
 _RK_CLIENT = None
 
 
-@app.route("/")
+@APP.route("/")
 def index():
+    """Function acting in the root directory."""
     return {"hello": "world"}
 
 
-@app.route("/hello")
+@APP.route("/hello")
 def hello_workshop():
+    """Function acting in the ~/hello/ directory"""
     return {"hello": "workshop"}
 
 
 def get_media_db():
+    """Function to access the media database."""
+    # pylint: disable=global-statement
     global _MEDIA_DB
     if _MEDIA_DB is None:
         _MEDIA_DB = db.DynamoMediaDB(
@@ -30,18 +38,21 @@ def get_media_db():
     return _MEDIA_DB
 
 
-def get_RK_CLIENT():
+def get_rk_client():
+    """Function to access the RK Client"""
+    # pylint: disable=global-statement
     global _RK_CLIENT
     if _RK_CLIENT is None:
         _RK_CLIENT = rekognition.RekognitonClient(boto3.client("rekognition"))
     return _RK_CLIENT
 
 
-@app.lambda_function()
-def detect_labels_on_image(event, context):
+@APP.lambda_function()
+def detect_labels_on_image(event):
+    """Function to detect the labels on an image"""
     bucket = event["Bucket"]
     key = event["Key"]
-    labels = get_RK_CLIENT().get_image_labels(bucket=bucket, key=key)
+    labels = get_rk_client().get_image_labels(bucket=bucket, key=key)
     get_media_db().add_media_file(key, media_type=db.IMAGE_TYPE, labels=labels)
 
 
@@ -50,15 +61,15 @@ def detect_labels_on_image(event, context):
 #
 # Here are a few more examples:
 #
-# @app.route('/hello/{name}')
+# @APP.route('/hello/{name}')
 # def hello_name(name):
 #    # '/hello/james' -> {"hello": "james"}
 #    return {'hello': name}
 #
-# @app.route('/users', methods=['POST'])
+# @APP.route('/users', methods=['POST'])
 # def create_user():
 #     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
+#     user_as_json = APP.current_request.json_body
 #     # We'll echo the json body back to the user in a 'user' key.
 #     return {'user': user_as_json}
 #
