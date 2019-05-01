aef5571a70fe156fed973d3d0183b5f5796a4a67
Alex Yarkosky <yarkoskya@allegheny.edu>
2019-05-01 00:07:03 -0400

    reformat with black

--- src/chalicelib/db.py
+++ src/chalicelib/db.py
@@ -3,8 +3,8 @@ from functools import reduce
 from boto3.dynamodb.conditions import Attr
 
 
-IMAGE_TYPE = 'image'
-VIDEO_TYPE = 'video'
+IMAGE_TYPE = "image"
+VIDEO_TYPE = "video"
 
 
 class MediaDB(object):
@@ -30,46 +30,32 @@ class DynamoMediaDB(MediaDB):
         filter_expression = None
         if startswith is not None:
             filter_expression = self._add_to_filter_expression(
-                filter_expression, Attr('name').begins_with(startswith)
+                filter_expression, Attr("name").begins_with(startswith)
             )
         if media_type is not None:
             filter_expression = self._add_to_filter_expression(
-                filter_expression, Attr('type').eq(media_type)
+                filter_expression, Attr("type").eq(media_type)
             )
         if label is not None:
             filter_expression = self._add_to_filter_expression(
-                filter_expression, Attr('labels').contains(label)
+                filter_expression, Attr("labels").contains(label)
             )
         if filter_expression:
-            scan_params['FilterExpression'] = filter_expression
+            scan_params["FilterExpression"] = filter_expression
         response = self._table.scan(**scan_params)
-        return response['Items']
+        return response["Items"]
 
     def add_media_file(self, name, media_type, labels=None):
         if labels is None:
             labels = []
-        self._table.put_item(
-            Item={
-                'name': name,
-                'type': media_type,
-                'labels': labels,
-            }
-        )
+        self._table.put_item(Item={"name": name, "type": media_type, "labels": labels})
 
     def get_media_file(self, name):
-        response = self._table.get_item(
-            Key={
-                'name': name,
-            },
-        )
-        return response.get('Item')
+        response = self._table.get_item(Key={"name": name})
+        return response.get("Item")
 
     def delete_media_file(self, name):
-        self._table.delete_item(
-            Key={
-                'name': name,
-            }
-        )
+        self._table.delete_item(Key={"name": name})
 
     def _add_to_filter_expression(self, expression, condition):
         if expression is None:
--- src/chalicelib/rekognition.py
+++ src/chalicelib/rekognition.py
@@ -7,47 +7,31 @@ class RekognitonClient(object):
 
     def get_image_labels(self, bucket, key):
         response = self._boto3_client.detect_labels(
-            Image={
-                'S3Object': {
-                    'Bucket': bucket,
-                    'Name': key
-                },
-            },
-            MinConfidence=50.0
+            Image={"S3Object": {"Bucket": bucket, "Name": key}}, MinConfidence=50.0
         )
-        return [label['Name'] for label in response['Labels']]
+        return [label["Name"] for label in response["Labels"]]
 
     def start_video_label_job(self, bucket, key, topic_arn, role_arn):
         response = self._boto3_client.start_label_detection(
-            Video={
-                'S3Object': {
-                    'Bucket': bucket,
-                    'Name': key
-                }
-            },
+            Video={"S3Object": {"Bucket": bucket, "Name": key}},
             ClientRequestToken=str(uuid.uuid4()),
-            NotificationChannel={
-                'SNSTopicArn': topic_arn,
-                'RoleArn': role_arn
-            },
-            MinConfidence=50.0
+            NotificationChannel={"SNSTopicArn": topic_arn, "RoleArn": role_arn},
+            MinConfidence=50.0,
         )
-        return response['JobId']
+        return response["JobId"]
 
     def get_video_job_labels(self, job_id):
         labels = set()
-        client_kwargs = {
-            'JobId': job_id,
-        }
+        client_kwargs = {"JobId": job_id}
         response = self._boto3_client.get_label_detection(**client_kwargs)
         self._collect_video_labels(labels, response)
-        while 'NextToken' in response:
-            client_kwargs['NextToken'] = response['NextToken']
+        while "NextToken" in response:
+            client_kwargs["NextToken"] = response["NextToken"]
             response = self._boto3_client.get_label_detection(**client_kwargs)
             self._collect_video_labels(labels, response)
         return list(labels)
 
     def _collect_video_labels(self, labels, response):
-        for label in response['Labels']:
-            label_name = label['Label']['Name']
+        for label in response["Labels"]:
+            label_name = label["Label"]["Name"]
             labels.add(label_name)
