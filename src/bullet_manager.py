4ad98269403d6c0b55ea9f39d76738873bf8c8cd
Zac Shaffer <shafferz@allegheny.edu>
2019-05-01 18:57:36 -0400

    Fixed pylint for bullet-manager.py

--- src/bullet_manager.py
+++ src/bullet_manager.py
@@ -1,9 +1,12 @@
+"""Module to manage the checklist and number of selected checks."""
 from bullet import Check, keyhandler
 from bullet.charDef import NEWLINE_KEY
 
 
 # taken from bullet/examples/check.py
 class MinMaxCheck(Check):
+    """Class to manage the most and least checks that can be made."""
+    # pylint: disable=keyword-arg-before-vararg
     def __init__(self, min_selections=0, max_selections=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.min_selections = min_selections
@@ -12,11 +15,14 @@ class MinMaxCheck(Check):
             self.max_selections = len(self.choices)
 
     @keyhandler.register(NEWLINE_KEY)
+    # pylint: disable=inconsistent-return-statements
     def accept(self):
+        """Function to apply a 'check'."""
         if self.valid():
             return super().accept()
 
     def valid(self):
+        """Function to validate and return all 'checks'."""
         return (
             self.min_selections
             <= sum(1 for c in self.checked if c)
@@ -25,6 +31,7 @@ class MinMaxCheck(Check):
 
 
 def get_choices(file_path):
+    """Function to load the checklist options."""
     file = open(file_path, "r")
     ret = [line.replace("\n", "") for line in file]
     while "" in ret:
@@ -33,6 +40,7 @@ def get_choices(file_path):
 
 
 def display_checklist(choices):
+    """Function to display the checklist to the user."""
     client = MinMaxCheck(
         prompt="Select Everything You Have Done (spacebar)\n"
         + "Press Enter When You Are Done Selecting",
