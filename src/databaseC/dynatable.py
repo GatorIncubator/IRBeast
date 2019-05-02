c9e6a0499282ea9ffda4a5c2769c03c73b770880
Zac Shaffer <shafferz@allegheny.edu>
2019-05-02 00:45:21 -0400

    Fixed pylint for dynatable.py

--- src/databaseC/dynatable.py
+++ src/databaseC/dynatable.py
@@ -1,9 +1,10 @@
+"""Wrapper for launching the dynamodb"""
 from __future__ import print_function
 import boto3
 
-dynamodb = boto3.resource("dynamodb")
+DYNAMODB = boto3.resource("dynamodb")
 
-table = dynamodb.create_table(
+TABLE = DYNAMODB.create_table(
     TableName="Irb",
     KeySchema=[
         {"AttributeName": "studentIDNumber", "KeyType": "HASH"},
@@ -17,6 +18,6 @@ table = dynamodb.create_table(
     ],
     ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
 )
-table.meta.client.get_waiter("table_exists").wait(TableName="Irb")
-print(table.item_count)
-print("Table status:", table.table_status)
+TABLE.meta.client.get_waiter("table_exists").wait(TableName="Irb")
+print(TABLE.item_count)
+print("Table status:", TABLE.table_status)
